a1e646fd54538fdec4107f8d14b555dc678f7d2d
Phi <buingocphinn@gmail.com>
2023-03-10 10:09:16 +0700

    update layout client

---
+++ app/Views/client/layout/index.php
@@ -0,0 +1,5 @@
+<?php
+require_once "./app/Views/client/layout/DataLayout/header.php";
+require_once "./app/Views/client/layout/DataLayout/home.php";
+require_once "./app/Views/client/layout/DataLayout/footer.php";
+?>
