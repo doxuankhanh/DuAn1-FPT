e714e3b15ce8a2289a0f0bc86dca50b03d1e257c
BuiThanh <thanhphuongmao4@gmail.com>
2023-03-11 22:35:07 +0700

    login/signin

---
+++ app/Views/client/layout/Pages/login.php
@@ -0,0 +1,14 @@
+<div class="div-login-form">
+    <div class="div-form-site">
+        <h3 class="h3-login">Đăng nhập</h3>
+        <form action="" class="form-login">
+            <label for="name">Tên đăng nhập</label>
+            <input type="text" class="input-form-login">
+            <label for="pwd">Mật khẩu</label>
+            <input type="password" class="input-form-login">
+            <label for="saveLgin">Ghi nhớ</label>
+            <input type="checkbox" class="input-checked-login">
+            <button type="submit">Đăng nhập</button>
+        </form>
+    </div>
+</div>
---
+++ app/Views/client/layout/Pages/signin.php
@@ -0,0 +1,19 @@
+<div class="div-login-form">
+    <div class="div-formSignIn-site">
+        <h3 class="h3-login">Đăng Ký</h3>
+        <form action="" class="form-login">
+            <label for="mail">Email :</label>
+            <input type="email" name="mail" class="input-form-login">
+            <label for="name">Tên đăng nhập :</label>
+            <input type="text" name="name" class="input-form-login">
+            <label for="pwd">Mật khẩu :</label>
+            <input type="password" name="pwd" class="input-form-login">
+            <label for="Repwd">Xác nhận mật khẩu :</label>
+            <input type="password" name="Repwd" class="input-form-login">
+            <label for="address">Địa chỉ :</label>
+            <input type="password" name="address" class="input-form-login">
+           
+            <button type="submit">Đăng Ký</button>
+        </form>
+    </div>
+</div>
