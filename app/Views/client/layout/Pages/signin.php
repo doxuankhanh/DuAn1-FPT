c2d461ed45d8031fc4a36142a5f052de1c06d97d
BuiThanh <thanhphuongmao4@gmail.com>
2023-03-12 00:34:19 +0700

    spanErr

--- app/Views/client/layout/Pages/signin.php
+++ app/Views/client/layout/Pages/signin.php
@@ -5,17 +5,22 @@
         <h3 class="h3-login">Đăng Ký</h3>
             <label for="mail">Email :</label>
             <input type="email" name="mail" class="input-form-login">
+            <span class="span-err" style="color:red;font-weight:bold;font-style:i">OkKhum</span>
             <label for="name">Tên đăng nhập :</label>
             <input type="text" name="name" class="input-form-login">
+            <span class="span-err" style="color:red;font-weight:bold;font-style:i">OkKhum</span>
             <label for="pwd">Mật khẩu :</label>
             <input type="password" name="pwd" class="input-form-login">
+            <span class="span-err" style="color:red;font-weight:bold;font-style:i">OkKhum</span>
             <label for="Repwd">Xác nhận mật khẩu :</label>
             <input type="password" name="Repwd" class="input-form-login">
+            <span class="span-err" style="color:red;font-weight:bold;font-style:i">OkKhum</span>
             <label for="phoneNum">Số điện thoại :</label>
             <input type="text" name="phoneNum" class="input-form-login">
+            <span class="span-err" style="color:red;font-weight:bold;font-style:i">OkKhum</span>
             <label for="address">Địa chỉ :</label>
             <input type="text" name="address" class="input-form-login">
-
+            <span class="span-err" style="color:red;font-weight:bold;font-style:i">OkKhum</span>
             <button type="submit">Đăng Ký</button>
         </form>
     </div>
