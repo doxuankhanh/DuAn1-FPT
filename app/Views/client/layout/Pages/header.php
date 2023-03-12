52f5e923215a17cff6e37c13fa9545df68908707
BuiThanh <thanhphuongmao4@gmail.com>
2023-03-12 22:19:21 +0700

    li Style/logoutSite

--- app/Views/client/layout/Pages/header.php
+++ app/Views/client/layout/Pages/header.php
@@ -18,11 +18,17 @@
             <div class="div-introduct">
                 <a class="anchor-a" href="#">GIỚI THIỆU</a>
                 <a class="anchor-a" href="#">LỊCH SỬ GIAO DỊCH</a>
-                <a class="anchor-a" href="#">kIỂM TRA ĐƠN HÀNG</a>
+                <a class="anchor-a" href="#">KIỂM TRA ĐƠN HÀNG</a>
             </div>
             <div class="div-login-site">
                 <a class="anchor-a" href="">ĐĂNG NHẬP</a>
                 <a class="anchor-a" href="#">ĐĂNG KÝ</a>
+                <div class="p-setting anchor-a " style="display: inline;" href="#"><img class="img-setting" src="./Public/images/product/settings-icon-13.png" alt="">
+                <div class="ul-setting">
+                    <p class="li-setting">TênNgườiDùng</p>
+                    <p class="li-setting"><a href="">LogOut</a></p>
+                </div>
+            </div>
             </div>
         </header>
         <div class="div-banner">
@@ -31,8 +37,7 @@
                 <a href="#" class="cart"></a>
                 <form action="" method="get">
                     <input type="text" class="search-box" placeholder="Tìm kiếm sách của bạn...">
-                    <input type="image" type="submit" name="search-btn" src="Public/images/product/searchbg.png" value="Tìm kiếm"
-                        class="search-btn">
+                    <input type="image" type="submit" name="search-btn" src="Public/images/product/searchbg.png" value="Tìm kiếm" class="search-btn">
                 </form>
             </div>
         </div>
@@ -40,15 +45,15 @@
             <ul class="ul-nav">
                 <li class="li-nav"><a href="#">DANH MỤC SÁCH</a>
                     <ul class="ul-child-list">
-                        <?php foreach($data['cates'] as $cate):?>
-                        <li class="li-child-list"><a href="<?= URL?>Book/bookFollowCategories/<?= $cate['id'] ?? ''?>"><?= $cate['cateName'] ?? ''?></a>
-                            <!-- <ul class="ul-child-last-list">
+                        <?php foreach ($data['cates'] as $cate) : ?>
+                            <li class="li-child-list"><a href="<?= URL ?>Book/bookFollowCategories/<?= $cate['id'] ?? '' ?>"><?= $cate['cateName'] ?? '' ?></a>
+                                <!-- <ul class="ul-child-last-list">
                                 <li class="li-child-last-list">Tác phẩm 1</li>
                                 <li class="li-child-last-list">Tác phẩm 2</li>
                                 <li class="li-child-last-list">Tác phẩm 3</li>
                             </ul> -->
-                        </li>
-                        <?php endforeach?>
+                            </li>
+                        <?php endforeach ?>
                     </ul>
                 </li>
                 <li class="li-nav"><a href="#">SÁCH KHUYẾN MẠI</a></li>
