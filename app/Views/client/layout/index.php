55e0d821aef7707e2d87238736d5561f28a82f0f
Tiep Tn <tatiep179@gmail.com>
2023-03-12 16:18:41 +0700

    up

--- app/Views/client/layout/index.php
+++ app/Views/client/layout/index.php
@@ -1,13 +1,6 @@
 <?php
 require_once "./app/Views/client/layout/Pages/header.php";
-if(isset($data['pages'])) {
-    require_once "./app/Views/". $data['pages'].".php";
-}else{
-    require_once "./app/Views/client/layout/Pages/home.php";
-}
-
-// require_once './app/Views/client/layout/bookDetail.php';
-
+require_once "./app/Views/client/layout/Pages/home.php";
 require_once "./app/Views/client/layout/Pages/footer.php";
 ?>
 ?>
