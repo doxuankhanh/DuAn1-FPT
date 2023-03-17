d750ae57f2ec5f8c3bd7dc10a7a25f1846f7c4e1
BuiThanh <thanhphuongmao4@gmail.com>
2023-03-17 23:37:23 +0700

    setting user

--- app/Views/client/layout/Pages/header.php
+++ app/Views/client/layout/Pages/header.php
@@ -27,20 +27,16 @@
                 <?php elseif (isset($_SESSION['username']) && $_SESSION['role'] == 0) : ?>
                     <a class="anchor-a" href="<?= URL ?>Home/destroy">ĐĂNG XUẤT</a>
                     <a class="anchor-a" href="<?= URL ?>Admin">ADMIN</a>
-                    <div class="p-setting anchor-a " style="display: inline;" href="#"><img class="img-setting" src="../../../../../../DuAn1-FPT/Public/images/product/settings-icon-13.png" alt="">
-                        <div class="ul-setting">
-                            <p class="li-setting"><?= $_SESSION['username'] ?? '' ?></p>
-                            <p class="li-setting"><a href="">LogOut</a></p>
-
-                        </div>
-                    </div>
+                
                 <?php else : ?>
                     <a href="#" style="position: relative;" class="anchor-a user">
                         Xin chào <?= isset($_SESSION['userID']) ? $_SESSION['username'] : '' ?>
-                        <ul class="subUser">
+                        <div class="p-setting" style="display: inline;" href="#"><img class="img-setting" src="../../../../../../DuAn1-FPT/Public/images/product/settings-icon-13.png" alt="">
+                        <ul class="ul-setting">
                             <li><a style="padding: 3px;" href="<?= URL ?>Home/updateUser/<?= $_SESSION['userID'] ?>">Thông tin tài khoản</a></li>
                             <li><a style="padding: 3px;" href="">Quên mật khẩu</a></li>
                         </ul>
+                        </div>
                     </a>
                     <!-- <a class="anchor-a" href="<?= URL ?>Home/updateUser/<?= $_SESSION['userID'] ?>">TÀI KHOẢN</a> -->
                     <a class="anchor-a" href="<?= URL ?>Home/destroy">ĐĂNG XUẤT</a>
