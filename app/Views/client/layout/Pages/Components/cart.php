85310b7564cf80e29c50b932344e2462976350d0
BuiThanh <thanhphuongmao4@gmail.com>
2023-03-17 23:26:10 +0700

    updateCart

--- app/Views/client/layout/Pages/Components/cart.php
+++ app/Views/client/layout/Pages/Components/cart.php
@@ -3,9 +3,54 @@
     <div class="div-main-cart">
         <?php if (count($data['carts']) > 0) : ?>
             <table class="table-cart">
-                <thead class="thead-cart">Giỏ hàng của bạn:</thead>
-                <thead>
-                    <tr>
+                <div class="div-header-cart">
+                    <header class="thead-cart">
+                        <div class="đây là msg thông báo khi người dùng đã xóa giỏ hàng">
+                            <?= $_SESSION['msgDelSuccessCart'] ?? '';
+                            unset($_SESSION['msgDelSuccessCart']) ?>
+                        </div>
+                        <div class="đây là msg thông báo khi người dùng update giỏ hàng thành công">
+                            <?= $_SESSION['msgUpdateCartSuccess'] ?? '';
+                            unset($_SESSION['msgUpdateCartSuccess']) ?>
+                        </div>
+                    </header>
+                    <div class="card">
+                        <form action="" class="form-cartCheckOut" method="post">
+                            <h3 class="h3-cart">Card Info</h3>
+                            <div class="div-input-infor">
+                                <input type="text" class="input-form-login" value="" name="firstName" required>
+                                <span class="span-label">Họ</span>
+                            </div>
+                            <div class="div-input-infor">
+                                <input type="text" class="input-form-login" value="" name="lastName" required>
+                                <span class="span-label">Tên</span>
+                            </div>
+
+                            <div class="div-input-infor">
+                                <input type="text" class="input-form-login" value="" name="address" required>
+                                <span class="span-label">Địa chỉ</span>
+                            </div>
+                            <div class="div-input-infor">
+                                <input type="text" class="input-form-login" value="" name="phoneNum" required>
+                                <span class="span-label">Số điện thoại</span>
+                            </div>
+                            <div class="div-input-checkout">
+                                <label for="checkFirst">Thanh toán khi nhận</label>
+                                <input type="radio" name="check" id="checkFirst" class="checkFirst" value="Thanh toán khi nhận">
+                                <label for="checkAfter">Thanh toán qua tài khoản</label>
+                                <input type="radio" name="check" id="checkAfter" class="checkAfter" value="Thanh toán qua tài khoản">
+                                
+                            </div>
+                        
+
+
+
+                        </form>
+                    </div>
+                </div>
+
+                <thead class="thead-title">
+                    <tr class="tr-title">
                         <td class="td-cart">Remove</td>
                         <td class="td-cart">Product</td>
                         <td class="td-cart">Price</td>
@@ -20,30 +65,22 @@
                             <td class="td-cart"><?= $cart['bookName'] ?? '' ?></td>
                             <td class="td-cart"><?= number_format($cart['price'] ?? '') ?></td>
                             <td class="td-cart">
-                                <form action="<?= URL ?>Home/updateCart/<?= $cart['cartID'] ?>" class="quantity" method="post">
-                                    <button onclick="this.parentNode.querySelector('input[type=number]').stepDown()" class="subtracts"></button>
+                                <form action="<?= URL ?>Home/updateCart/<?= $cart['cartID'] ?>" class="form-CountPrd" method="post">
+                                    <button onclick="this.parentNode.querySelector('input[type=number]').stepDown()" class="subtracts-cart"></button>
                                     <input type="number" value="<?= $cart['quantity'] ?>" style="-webkit-appearance: none;" min="1" name="quantity">
-                                    <button onclick="this.parentNode.querySelector('input[type=number]').stepUp()" class="plus"></button>
+                                    <button onclick="this.parentNode.querySelector('input[type=number]').stepUp()" class="plus-cart"></button>
                                     <button class="updateBtn" type="submit" name="btn-updateCart" style="cursor: pointer;">Update</button>
                                 </form>
                             </td>
                             <td class="td-cart"><img src="../../../../../../../DuAn1-FPT/Public/upload/<?= $cart['image'] ?? '' ?>" alt=""></td>
                         </tr>
                     <?php endforeach ?>
-                    <button class="button-paying">Thanh toán</button>
                 </tbody>
-                <div class="đây là msg thông báo khi người dùng đã xóa giỏ hàng">
-                    <?= $_SESSION['msgDelSuccessCart'] ?? '';
-                    unset($_SESSION['msgDelSuccessCart']) ?>
-                </div>
-                <div class="đây là msg thông báo khi người dùng update giỏ hàng thành công">
-                    <?= $_SESSION['msgUpdateCartSuccess'] ?? '';
-                    unset($_SESSION['msgUpdateCartSuccess']) ?>
-                </div>
             </table>
         <?php else : ?>
             <div style="text-align: center;">Không có sản phẩm nào trong giỏ hàng của bạn!</div>
         <?php endif ?>
+        <button class="button-paying">Thanh toán</button>
     </div>
 </div>
 <?php require_once "./app/Views/client/layout/Pages/footer.php"; ?>
