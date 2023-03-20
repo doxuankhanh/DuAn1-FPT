e281f785bbb031e094a3def3b4b946088922efd9
Phi <buingocphinn@gmail.com>
2023-03-20 09:19:00 +0700

    change somethings

--- app/Views/client/layout/Pages/Components/checkOut.php
+++ app/Views/client/layout/Pages/Components/checkOut.php
@@ -3,7 +3,7 @@
 <div class="div-checkout-container">
     <div class="div-checkout-main">
         <h2 class=" h2-checkout">Đặt hàng thành công!<br>Cảm ơn bạn đã mua hàng</h2>
-        <form action="<?= URL?>Home/checkOut" method="post">
+        <form action="<?= URL ?>Home/checkOut" method="post">
             <div class="div-infor-checkout">
                 <h4 style="">Thông tin người mua</h4>
                 <div class="div-infor site">
@@ -14,10 +14,10 @@
                         <p class="p-infor">Địa chỉ</p>
                     </div>
                     <div class="div-infor-main">
-                        <p class="p-infor"><input type="text" name="username" id="" value="<?= $_SESSION['username'] ?? ''?>" disabled></p>
-                        <p class="p-infor"><input type="text" name="email" id="" value="<?= $_SESSION['email'] ?? ''?>" disabled></p>
-                        <p class="p-infor"><input type="text" name="phoneNumber" id="" value="<?= $_SESSION['phone'] ?? ''?>" disabled></p>
-                        <p class="p-infor"><input type="text" name="address" id="" value="<?= $_SESSION['address'] ?? ''?>" disabled></p>
+                        <p class="p-infor"><input type="text" name="username" id="" value="<?= $_SESSION['username'] ?? '' ?>" disabled></p>
+                        <p class="p-infor"><input type="text" name="email" id="" value="<?= $_SESSION['email'] ?? '' ?>" disabled></p>
+                        <p class="p-infor"><input type="text" name="phoneNumber" id="" value="<?= $_SESSION['phone'] ?? '' ?>" disabled></p>
+                        <p class="p-infor"><input type="text" name="address" id="" value="<?= $_SESSION['address'] ?? '' ?>" disabled></p>
                     </div>
                     <h2>Thông tin sản phẩm</h2>
                     <table class="table-checkout">
@@ -30,18 +30,18 @@
                         </thead>
                         <tbody>
                             <?php $total = 0;
-                            foreach($data['carts'] as $key => $value): 
-                            $subtotal = ($value['quantity'] * $value['price']);
-                            $total += $subtotal;
+                            foreach ($data['carts'] as $key => $value) :
+                                $subtotal = ($value['quantity'] * $value['price']);
+                                $total += $subtotal;
                             ?>
-                            <tr>
-                                <td><?= $key+1?></td>
-                                <td><?= $value['bookName'] ?? ''?></td>
-                                <td><?= number_format($value['price'] ?? '')?></td>
-                                <td><?= $value['quantity'] ?? ''?></td>
-                                <td><?= number_format($subtotal)?></td>
-                            </tr>
-                            <?php endforeach?>
+                                <tr>
+                                    <td><?= $key + 1 ?></td>
+                                    <td><?= $value['bookName'] ?? '' ?></td>
+                                    <td><?= number_format($value['price'] ?? '') ?></td>
+                                    <td><?= $value['quantity'] ?? '' ?></td>
+                                    <td><?= number_format($subtotal) ?></td>
+                                </tr>
+                            <?php endforeach ?>
                             <tr>
                                 <td></td>
                                 <td></td>
@@ -54,14 +54,11 @@
                                 <td></td>
                                 <td></td>
                                 <td><strong>Tổng cộng</strong></td>
-                                <td><?= number_format($total)?></td>
+                                <td><?= number_format($total) ?></td>
                             </tr>
                         </tbody>
                     </table>
                 </div>
-                <h2 class="h2-checkout">
-                    <button>Xác nhận thanh toán </button>
-                </h2>
             </div>
             <h2 class="h2-checkout">
                 <button type="submit">Xác nhận thanh toán </button>
