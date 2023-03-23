14e9ca2a51685a4e4e76794ce3fb114062736bf0
thanhdtph29208 <thanhdtph29208@fpt.edu.vn>
2023-03-23 16:47:14 +0700

    .

---
+++ app/Views/client/layout/Pages/Components/DataLayout/pay.php
@@ -0,0 +1,19 @@
+<?php require_once "./app/Views/client/layout/Pages/header.php"; ?>
+
+<div class="div-checkout-container">
+    <div class="div-checkout-main">
+    <form action="" method="post">
+            <h2>Tài khoản đã liên kết</h2>
+            <div style="margin-top: 10px;">
+                <h4>Ngân hàng: MB Bank</h4>
+                <h4>Tài khoản của bạn: 0869833156</h4>
+                <h4>Tên tài khoản: ĐỒNG THÚY THANH</h4>
+            </div>
+            <div class="h2-checkout" style="margin-top:15px;">
+                <button type="submit" name="btn">Xác nhận thanh toán</button>
+            </div>
+    </form>
+    </div>
+</div>
+
+<?php require_once "./app/Views/client/layout/Pages/footer.php"; ?>
